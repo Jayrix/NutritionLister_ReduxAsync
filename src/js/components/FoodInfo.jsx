05fbff55437cfd07d7f5908c120e075117f41e91
Jakub Kozlik <kozlik.jakub@gmail.com>
2018-01-14 23:46:53 +0100

    made food presentational components

--- src/js/components/FoodInfo.jsx
+++ src/js/components/FoodInfo.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const FoodInfo = (props) => (
-    <p></p>
+export const FoodInfo = (props) => (
+    <li>
+        <span>
+            {props.foodName} :
+        </span>
+        <ul>
+            <li>{props.foodKcals}</li>
+        </ul>
+    </li>
 )
---
+++ src/js/components/FoodsList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import FoodInfo from 'FoodInfo.jsx';
+
+
+let FoodsList = (props) => (
+    <ul>
+        {props.foodsArray.map((food, index) => <FoodInfo key={index} foodName={props.foodName} foodKcals={props.foodKcals}/>)}
+    </ul>
+)
